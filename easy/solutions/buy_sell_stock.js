e350a4d12c7755d8c585482d519c521e4a82f44b
Emily Ringoen <emilyaringoen@gmail.com>
2019-11-03 10:56:43 -0700

    add remaining easy solutions

--- easy/solutions/buy_sell_stock.js
+++ easy/solutions/buy_sell_stock.js
@@ -5,3 +5,12 @@ https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solution/
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/?currentPage=1&orderBy=hot&query=
 *
 */
+var maxProfit = function(prices) {
+  var ans = 0;
+  var curLowest = prices[0];
+  for (var i = 1; i < prices.length; i++){
+      curLowest = Math.min(curLowest, prices[i]);
+      ans = Math.max(prices[i] - curLowest, ans);
+  }
+  return ans;
+};
--- easy/solutions/valid_parens.js
+++ easy/solutions/valid_parens.js
@@ -5,3 +5,23 @@ https://leetcode.com/problems/valid-parentheses/solution/
 https://leetcode.com/problems/valid-parentheses/discuss/?currentPage=1&orderBy=hot&query=
 *
 */
+function isValid(s) {
+    let map = {
+        ")": "(",
+        "]": "[",
+        "}": "{"
+    }
+    let arr = [];
+    for(let i = 0; i < s.length; i ++){
+        if(s[i] === "(" || s[i] === "[" || s[i] === "{"){
+            arr.push(s[i]);
+        }
+        else{
+            if(arr[arr.length - 1] === map[s[i]]){
+                arr.pop();
+            }
+            else return false;
+        }
+    }
+    return arr.length === 0 ? true : false;
+};
