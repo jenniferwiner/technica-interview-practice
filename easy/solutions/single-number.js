924eee87c4322388428a26bc448fd9e82f4f6311
Emily Ringoen <emilyaringoen@gmail.com>
2019-11-05 17:43:43 -0700

    fix solution for single number

--- easy/solutions/single-number.js
+++ easy/solutions/single-number.js
@@ -1,17 +1,12 @@
-/*
-*
-* For solution in other languages:
-https://leetcode.com/problems/single-number/
-https://leetcode.com/problems/single-number/discuss/?currentPage=1&orderBy=hot&query=
-*
-*/
+
+
+
 
 var singleNumber = function(nums) {
 
     nums.sort((a,b)=>a-b);
     for(var i=0; i< nums.length; i++){
-        nums[i] !== nums[i+1]
-        if(nums[i] !== nums[i+1]){
+        if(nums[i] !== nums[i+1] && nums[i] !== nums[i-1]){
             return nums[i];
         }
         i++;
